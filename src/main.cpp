416bdf858a94962684a7e6a73ac0dfd8ea92734e
n0F4x <ggabor2002@gmail.com>
2022-12-28 19:18:05 +0100

    better example

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,10 @@ int main() {
 		.add_stage(Stage::create()
 			.add_system([](Controller& controller) {
 				std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-	std::cout << "Stage 1\n";
+	std::cout << "Stage 1 - first\n";
+	controller.quit(); })
+			.add_system([](Controller& controller) {
+	std::cout << "Stage 1 - second\n";
 	controller.quit(); }))
 		.add_stage(Stage::create()
 			.add_system([](Controller& controller) { std::cout << "Stage 2\n"; controller.quit(); }))
