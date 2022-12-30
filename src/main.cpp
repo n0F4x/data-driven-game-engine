08ff92ac28a171b138a1969fd7fc1b4782c6beeb
Gabor Galgoczy <ggabor@gmail.com>
2022-12-30 19:33:14 +0100

    magic '+' for lamdas

--- src/main.cpp
+++ src/main.cpp
@@ -17,17 +17,17 @@ auto main() -> int {
     App::create()
         .add_name("My game engine")
         .add_state(State::create<"MyState"_hs>()
-            .on_enter([] { std::cout << "Entered\n"; })
+            .on_enter(+[] { std::cout << "Entered\n"; })
             .on_exit(exited))
         .add_stage(Stage::create()
-            .add_system([](Controller& controller) {
+            .add_system(+[](Controller& controller) {
                 std::this_thread::sleep_for(std::chrono::milliseconds(1000));
                 std::cout << "Stage 1 - first\n";
                 controller.quit(); })
-            .add_system([](Controller& controller) {
+            .add_system(+[](Controller& controller) {
                 std::cout << "Stage 1 - second\n";
                 controller.quit(); }))
         .add_stage(Stage::create()
-            .add_system([](Controller& controller) { std::cout << "Stage 2\n"; controller.quit(); }))
+            .add_system(+[](Controller& controller) { std::cout << "Stage 2\n"; controller.quit(); }))
         .build().run();
 }
