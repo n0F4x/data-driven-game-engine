11d88cf2be98f2af326293424c303fda7b736083
Gabor Galgoczy <ggabor@gmail.com>
2022-12-31 18:59:30 +0100

    removing old vendor

---
+++ include/engine/Renderer.hpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+#include "engine/Stage.hpp"
+
+
+class Renderer {
+public:
+    void render() {
+
+    }
+
+private:
+    std::vector<Stage> stages;
+};
---
+++ include/engine/Scheduler.hpp
@@ -0,0 +1,3 @@
+#include "engine/Renderer.hpp"
+
+
