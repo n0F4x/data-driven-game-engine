0d70685cba755d9c1d754cf70b1344a2009f33f4
Gabor Galgoczy <ggabor@gmail.com>
2023-01-25 12:47:52 +0100

    changed Action to std::function

--- include/framework/State.hpp
+++ include/framework/State.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <functional>
 #include <utility>
 
 #include <gsl/pointers>
@@ -15,7 +16,7 @@ class State final {
     ///----------------///
 
 public:
-    using Action = gsl::not_null<void (*)()>;
+    using Action = std::function<void()>;
 
 private:
     class Builder;
@@ -23,35 +24,32 @@ private:
 
     friend App;
 
-    constexpr static void empty_action() noexcept { /*empty by default*/
-    }
-
 public:
     ///------------------------------///
     ///  Constructors / Destructors  ///
     ///------------------------------///
-    [[nodiscard]] constexpr State(const State&) = delete;
-    [[nodiscard]] constexpr State(State&&) noexcept = default;
+    [[nodiscard]] State(const State&) = delete;
+    [[nodiscard]] State(State&&) noexcept = default;
 
     ///--------------------///
     ///  Member functions  ///
     ///--------------------///
-    [[nodiscard]] constexpr auto get_id() const noexcept;
-    constexpr void entered() const noexcept;
-    constexpr void exited() const noexcept;
+    [[nodiscard]] auto get_id() const noexcept -> Id;
+    void entered() const noexcept;
+    void exited() const noexcept;
 
     ///------------------///
     ///  Static helpers  ///
     ///------------------///
     template <Id id>
         requires(id != 0)
-    [[nodiscard]] constexpr static auto create() noexcept;
-    [[nodiscard]] constexpr static auto invalid_state() noexcept
+    [[nodiscard]] static auto create() noexcept -> Builder;
+    [[nodiscard]] static auto invalid_state() noexcept
         -> gsl::not_null<const State*>;
-    [[nodiscard]] constexpr static auto invalid(const State& state) noexcept;
+    [[nodiscard]] static auto invalid(const State& state) noexcept -> bool;
 
 private:
-    [[nodiscard]] constexpr explicit State(Id id = {}) noexcept : id{ id } {}
+    [[nodiscard]] explicit State(Id id = {}) noexcept : id{ id } {}
 
     ///--------------------///
     ///  Member variables  ///
@@ -59,8 +57,8 @@ private:
     static const State s_invalid_state;
 
     Id id{};
-    Action onEnter = empty_action;
-    Action onExit = empty_action;
+    Action onEnter;
+    Action onExit;
 };
 
 class State::Builder final : public BuilderBase<State> {
@@ -73,52 +71,12 @@ public:
     ///--------------------///
     ///  Member functions  ///
     ///--------------------///
-    [[nodiscard]] constexpr auto on_enter(Action callback) noexcept;
-    [[nodiscard]] constexpr auto on_exit(Action callback) noexcept;
+    [[nodiscard]] auto on_enter(Action&& callback) noexcept -> Self;
+    [[nodiscard]] auto on_exit(Action&& callback) noexcept -> Self;
 };
-
-/// ////////////////////// ///
-///     IMPLEMENTATION     ///
-/// ////////////////////// ///
-
-constexpr inline State State::s_invalid_state;
 
 template <Id id>
     requires(id != 0)
-[[nodiscard]] constexpr auto State::create() noexcept {
+auto State::create() noexcept -> State::Builder {
     return Builder{ id };
-}
-
-[[nodiscard]] constexpr gsl::not_null<const State*>
-State::invalid_state() noexcept {
-    return &s_invalid_state;
-}
-
-[[nodiscard]] constexpr auto State::invalid(const State& state) noexcept {
-    return state.id == 0;
-}
-
-[[nodiscard]] constexpr auto State::get_id() const noexcept {
-    return id;
-}
-
-constexpr void State::entered() const noexcept {
-    onEnter();
-}
-
-constexpr void State::exited() const noexcept {
-    onExit();
-}
-
-[[nodiscard]] constexpr auto
-State::Builder::on_enter(Action callback) noexcept {
-    draft().onEnter = callback;
-
-    return std::move(*this);
-}
-
-[[nodiscard]] constexpr auto State::Builder::on_exit(Action callback) noexcept {
-    draft().onExit = callback;
-
-    return std::move(*this);
 }
---
+++ src/framework/State.cpp
@@ -0,0 +1,40 @@
+#include "framework/State.hpp"
+
+
+const State State::s_invalid_state;
+
+gsl::not_null<const State*> State::invalid_state() noexcept {
+    return &s_invalid_state;
+}
+
+auto State::invalid(const State& state) noexcept -> bool {
+    return state.id == 0;
+}
+
+auto State::get_id() const noexcept -> Id {
+    return id;
+}
+
+void State::entered() const noexcept {
+    if (onEnter) {
+        onEnter();
+    }
+}
+
+void State::exited() const noexcept {
+    if (onExit) {
+        onExit();
+    }
+}
+
+auto State::Builder::on_enter(Action&& callback) noexcept -> Self {
+    draft().onEnter = std::move(callback);
+
+    return std::move(*this);
+}
+
+auto State::Builder::on_exit(Action&& callback) noexcept -> Self {
+    draft().onExit = std::move(callback);
+
+    return std::move(*this);
+}
