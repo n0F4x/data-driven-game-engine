73d81cda6023a333c97e6138eb76ed021983485e
Gabor Galgoczy <ggabor2002@gmail.com>
2023-06-13 00:02:25 +0200

    added basic Device implementation

--- src/engine/renderer/Device.hpp
+++ src/engine/renderer/Device.hpp
@@ -12,11 +12,21 @@ public:
     explicit Device(vk::raii::Instance&&        t_instance,
                     const vk::raii::SurfaceKHR& t_surface);
 
+    auto physical_device() -> const vk::raii::PhysicalDevice&;
+    auto device() -> const vk::raii::Device&;
+
+    auto graphics_queue() -> const vk::raii::Queue&;
+    auto present_queue() -> const vk::raii::Queue&;
+
 private:
     ///-------------///
     ///  Variables  ///
     ///-------------///
-    vk::raii::Instance m_instance;
+    vk::raii::Instance       m_instance;
+    vk::raii::PhysicalDevice m_physical_device;
+    vk::raii::Device         m_device;
+    vk::raii::Queue          m_graphics_queue;
+    vk::raii::Queue          m_present_queue;
 };
 
 }   // namespace engine::renderer
