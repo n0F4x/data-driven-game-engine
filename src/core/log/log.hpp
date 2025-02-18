068a215aee8deb0037facd277bd99acdde5a39fd
Galgóczy Gábor <ggabor2002@gmail.com>
2025-02-18 18:01:08 +0100

    Set default log level to debug in debug

--- src/core/log/log.hpp
+++ src/core/log/log.hpp
@@ -18,9 +18,9 @@ auto logger() -> spdlog::logger&;
 #define ENGINE_LOG_LEVEL_TRACE    6
 
 #if ENGINE_LOG_LEVEL >= ENGINE_LOG_LEVEL_CRITICAL
-  #define ENGINE_LOG_CRITICAL(...)                                           \
-      SPDLOG_LOGGER_CALL(                                                    \
-          &core::log::internal::logger, spdlog::level::critical, __VA_ARGS__ \
+  #define ENGINE_LOG_CRITICAL(...)                                             \
+      SPDLOG_LOGGER_CALL(                                                      \
+          &core::log::internal::logger(), spdlog::level::critical, __VA_ARGS__ \
       )
 #else
   #define ENGINE_LOG_CRITICAL(...) (void)0
@@ -28,35 +28,39 @@ auto logger() -> spdlog::logger&;
 
 #if ENGINE_LOG_LEVEL >= ENGINE_LOG_LEVEL_ERROR
   #define ENGINE_LOG_ERROR(...) \
-      SPDLOG_LOGGER_CALL(&core::log::internal::logger, spdlog::level::error, __VA_ARGS__)
+      SPDLOG_LOGGER_CALL(&core::log::internal::logger(), spdlog::level::err, __VA_ARGS__)
 #else
   #define ENGINE_LOG_ERROR(...) (void)0
 #endif
 
 #if ENGINE_LOG_LEVEL >= ENGINE_LOG_LEVEL_WARNING
   #define ENGINE_LOG_WARNING(...) \
-      SPDLOG_LOGGER_CALL(&core::log::internal::logger, spdlog::level::warn, __VA_ARGS__)
+      SPDLOG_LOGGER_CALL(&core::log::internal::logger(), spdlog::level::warn, __VA_ARGS__)
 #else
   #define ENGINE_LOG_WARNING(...) (void)0
 #endif
 
 #if ENGINE_LOG_LEVEL >= ENGINE_LOG_LEVEL_INFO
   #define ENGINE_LOG_INFO(...) \
-      SPDLOG_LOGGER_CALL(&core::log::internal::logger, spdlog::level::info, __VA_ARGS__)
+      SPDLOG_LOGGER_CALL(&core::log::internal::logger(), spdlog::level::info, __VA_ARGS__)
 #else
   #define ENGINE_LOG_INFO(...) (void)0
 #endif
 
 #if ENGINE_LOG_LEVEL >= ENGINE_LOG_LEVEL_DEBUG
-  #define ENGINE_LOG_DEBUG(...) \
-      SPDLOG_LOGGER_CALL(&core::log::internal::logger, spdlog::level::debug, __VA_ARGS__)
+  #define ENGINE_LOG_DEBUG(...)                                             \
+      SPDLOG_LOGGER_CALL(                                                   \
+          &core::log::internal::logger(), spdlog::level::debug, __VA_ARGS__ \
+      )
 #else
   #define ENGINE_LOG_DEBUG(...) (void)0
 #endif
 
 #if ENGINE_LOG_LEVEL >= ENGINE_LOG_LEVEL_TRACE
-  #define ENGINE_LOG_TRACE(...) \
-      SPDLOG_LOGGER_CALL(&core::log::internal::logger, spdlog::level::trace, __VA_ARGS__)
+  #define ENGINE_LOG_TRACE(...)                                             \
+      SPDLOG_LOGGER_CALL(                                                   \
+          &core::log::internal::logger(), spdlog::level::trace, __VA_ARGS__ \
+      )
 #else
   #define ENGINE_LOG_TRACE(...) (void)0
 #endif
