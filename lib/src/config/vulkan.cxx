1f64125f957dfbb8c8a338c124e2ea8d03657f0e
Galgóczy Gábor <ggabor2002@gmail.com>
2025-08-10 17:35:38 +0200

    Move `VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE` into its own file

--- lib/src/config/vulkan.cxx
+++ lib/src/config/vulkan.cxx
@@ -2,11 +2,9 @@ module;
 
 #include <vulkan/vulkan_hpp_macros.hpp>
 
-import vulkan_hpp;
-
-VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
-
 module ddge.config.vulkan;
+
+import vulkan_hpp;
 
 namespace ddge::config::vulkan {
 
