e46a2294edaa017bbc8761206773f8d58024f42c
Galgóczy Gábor <ggabor2002@gmail.com>
2025-08-14 19:03:25 +0200

    Fix third-party recipes

--- third-party/recipes/sfml/all/conanfile.py
+++ third-party/recipes/sfml/all/conanfile.py
@@ -5,7 +5,7 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import is_apple_os
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
-from conan.tools.files import modules.apply_conandata_patches, export_conandata_patches, get, copy, rmdir
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, rmdir
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
 
 
