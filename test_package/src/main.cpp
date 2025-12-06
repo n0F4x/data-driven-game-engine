e7e3245d3be5e01e474c9dcccc1a662e9ff522bd
Galgóczy Gábor <ggabor2002@gmail.com>
2025-12-06 17:38:25 +0100

    Fix test_package not compiling

--- test_package/src/main.cpp
+++ test_package/src/main.cpp
@@ -3,7 +3,7 @@ import ddge.prelude;
 auto main() -> int
 {
     return ddge::app::create()
-        .plug_in(ddge::plugins::Resources{})
+        .plug_in(ddge::resources::Plugin{})
         .insert_resource(int{})
         .build()
         .resource_manager.at<int>();
